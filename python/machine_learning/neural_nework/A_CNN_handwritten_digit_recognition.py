38a1f123ed06f672b87b20a2eb9804bcc4afcde5
zhuojun.lai <zhuojun.lai@dorabot.com>
2025-11-01 09:10:06 +0800

    [chore] Add comment

--- python/machine_learning/neural_nework/A_CNN_handwritten_digit_recognition.py
+++ python/machine_learning/neural_nework/A_CNN_handwritten_digit_recognition.py
@@ -10,31 +10,30 @@ from torchvision.transforms import ToTensor
 
 # Load data
 train_data = datasets.MNIST(
-    root = 'data',
-    train = True,
-    transform = ToTensor(),
-    download = True
+    root="data", train=True, transform=ToTensor(), download=True
 )
 
+# Each image is 28x28 pixels.
+
 test_data = datasets.MNIST(
-    root = 'data',
-    train = False,
-    transform = ToTensor(),
-    download = True
+    root="data", train=False, transform=ToTensor(), download=True  # test data
 )
 loaders = {
-    'train': DataLoader(train_data, batch_size=100, shuffle=True, num_workers=1),
-    'test': DataLoader(test_data, batch_size=100, shuffle=True, num_workers=1)
+    "train": DataLoader(train_data, batch_size=100, shuffle=True, num_workers=1),
+    "test": DataLoader(test_data, batch_size=100, shuffle=True, num_workers=1),
 }
+
 
 # Create Convolutional Neural Network model
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        
+
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        # Randomly zero out entire channels (a channel is a 2D feature map, e.g., the $j$-th channel of the $i$-th sample in the batched input is a 2D tensor $\text{input}[i, j]$). Each channel will be zeroed out independently on every forward call with probability p using samples from a Bernoulli distribution.
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
+        # Applies a linear transformation to the incoming data: $y = xA^T + b$
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
 
@@ -48,15 +47,17 @@ class CNN(nn.Module):
 
         return F.softmax(x)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = CNN().to(device)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 loss_fn = nn.CrossEntropyLoss()
 
+
 # Creating the Dataset Training Mode
 def train(epoch):
     model.train()
-    for batch_idx, (data, target) in enumerate(loaders['train']):
+    for batch_idx, (data, target) in enumerate(loaders["train"]):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
@@ -64,21 +65,27 @@ def train(epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % 20 == 0:
-            print(f"Train Epoch: {epoch} [{batch_idx * len(data)} / {len(loaders['train'].dataset)} ({100 * batch_idx / len(loaders['train']):0f}%)]\t{loss.item():.6f}")
+            print(
+                f"Train Epoch: {epoch} [{batch_idx * len(data)} / {len(loaders['train'].dataset)} ({100 * batch_idx / len(loaders['train']):0f}%)]\t{loss.item():.6f}"
+            )
+
 
 def test():
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in loaders['test']:
+        for data, target in loaders["test"]:
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += loss_fn(output, target).item()
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
-    test_loss /= len(loaders['test'].dataset)
-    print(f"\nTest set: Average loss: {test_loss: 0.4f}, Accuracy {correct}/{len(loaders['test'].dataset)}  ({100 * correct / len(loaders['test'].dataset):.0f}%\n")
+    test_loss /= len(loaders["test"].dataset)
+    print(
+        f"\nTest set: Average loss: {test_loss: 0.4f}, Accuracy {correct}/{len(loaders['test'].dataset)}  ({100 * correct / len(loaders['test'].dataset):.0f}%\n"
+    )
+
 
 for epoch in range(1, 10):
     train(epoch)
